7531ea63945145e318acf4fea2539ad976e850b4
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-08-03 16:06:12 +0100

    initial commit created main.dart, input_page for home, reusable_card component, and icon_content component

---
+++ lib/input_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const cardColor = Color(0xFF1D1E33);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: cardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: cardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: cardColor,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: cardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: cardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 70.0,
+          )
+        ],
+      ),
+    );
+  }
+}
